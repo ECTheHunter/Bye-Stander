982b8fd37b3a7353bd92c8337b9c460cbfe6fa9e
eliasAinsworth7 <bavut08@hotmail.com>
2025-01-25 11:46:29 +0300

    Dialog is done

---
+++ Assets/conversationStarter.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using DialogueEditor;
+using System;
+public class conversationStarter : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private NPCConversation conversation;
+    [SerializeField] private NPCConversation conversation1;
+    private bool isMadeBefore = false;
+    private int count = 0;
+
+    private void OnTriggerStay(Collider other){
+
+        if(other.CompareTag("Player")){
+
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                if(isMadeBefore)
+                {
+                    
+                    ConversationManager.Instance.StartConversation(conversation1);
+                }
+                else
+                {
+                    ConversationManager.Instance.StartConversation(conversation);
+                    count++;
+                    if(count == 3)
+                    {
+                        isMadeBefore = true;
+                        
+                    }
+                }
+                
+            }
+        }
+    }
+    
+}
