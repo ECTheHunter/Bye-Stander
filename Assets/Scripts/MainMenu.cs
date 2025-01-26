99d127a34de0a11ca3fd4d6ace1758f54d67cee1
eliasAinsworth7 <bavut08@hotmail.com>
2025-01-26 10:30:32 +0300

    adjusted

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Play()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("RealLife");
     }
 
     // Update is called once per frame
